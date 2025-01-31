7c6220811f84fd215d14bd9152789f46523a9dcf
Anushek Prasal <anushekprasal@gmail.com>
2025-01-31 15:45:57 +0100

    Settings: Introduce automated Sleep Mode [2/2]
    Signed-off-by: Anushek Prasal <anushekprasal@gmail.com>
    Signed-off-by: cjh1249131356 <cjh1249131356@gmail.com>
    Signed-off-by: minaripenguin <minaripenguin@users.noreply.github.com>

--- src/com/android/settings/core/gateway/SettingsGateway.java
+++ src/com/android/settings/core/gateway/SettingsGateway.java
@@ -122,6 +122,7 @@ import com.android.settings.fuelgauge.batterysaver.BatterySaverScheduleSettings;
 import com.android.settings.fuelgauge.batterysaver.BatterySaverSettings;
 import com.android.settings.fuelgauge.batteryusage.PowerUsageAdvanced;
 import com.android.settings.fuelgauge.batteryusage.PowerUsageSummary;
+import com.android.settings.fuelgauge.sleepmode.SleepMode;
 import com.android.settings.gestures.ButtonNavigationSettingsFragment;
 import com.android.settings.gestures.DoubleTapPowerSettings;
 import com.android.settings.gestures.DoubleTapScreenSettings;
@@ -431,6 +432,7 @@ public class SettingsGateway {
             BlackironSettings.class.getName(),
             RunningServices.class.getName(),
             SmartPixels.class.getName(),
+            SleepMode.class.getName(),
     };
 
     public static final String[] SETTINGS_FOR_RESTRICTED = {
@@ -480,5 +482,6 @@ public class SettingsGateway {
             Settings.BlackironSettings.class.getName(),
             Settings.DevRunningServicesActivity.class.getName(),
             Settings.SmartPixelsActivity.class.getName(),
+            Settings.SleepModeActivity.class.getName()
     };
 }
