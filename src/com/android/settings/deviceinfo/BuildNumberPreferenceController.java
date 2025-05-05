ProjectMatrixx/android_packages_apps_Settings

Settings: Update build number before display
* Triggers some users.

Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

src/com/android/settings/deviceinfo/BuildNumberPreferenceController.java
@@ -83,7 +83,8 @@ public class BuildNumberPreferenceController extends BasePreferenceController im
 
     @Override
     public CharSequence getSummary() {
-        return BidiFormatter.getInstance().unicodeWrap(Build.DISPLAY);
+        String display = Build.DISPLAY.replace("lineage_", "crdroid_");
+        return BidiFormatter.getInstance().unicodeWrap(display);
     }
 
     @Override

Black-Iron-Project/packages_apps_Settings

src/com/android/settings/deviceinfo/BuildNumberPreferenceController.java
@@ -83,7 +83,8 @@ public class BuildNumberPreferenceController extends BasePreferenceController im
 
     @Override
     public CharSequence getSummary() {
-        return BidiFormatter.getInstance().unicodeWrap(Build.DISPLAY);
+        String display = Build.DISPLAY.replace("lineage_", "blackiron");
+        return BidiFormatter.getInstance().unicodeWrap(display);
     }
 
     @Override